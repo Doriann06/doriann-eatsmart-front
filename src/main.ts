8f40d0bc26f8912c5f21b9fa58f251e53c3286c5
Doriann Bachelet <doriann.bak@gmail.com>
2026-03-05 00:23:00 +0100

    Besoin n°2 : Présentation visuelle

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+// Fichier main.ts
+import './style.css'
 interface Plat{
   id:number;
   nom:string;
@@ -10,3 +12,15 @@ const carte:Plat[]=[
   {id:3 ,nom:'Margherita 23cm',prix:7.9,description:"sauce tomate premium,origan,huile d'olive extra vierge,mozzarella"}
 ]
 console.log(carte);
+const appDiv=document.querySelector<HTMLDivElement>('#app');
+if(appDiv){
+  appDiv.innerHTML=`
+    ${carte.map(p=>`
+      <div class=card>
+      <h3>${p.nom}</h3>
+      <p>${p.description}<p>
+      <p><strong>Prix: ${p.prix}€</strong></p>
+      </div>
+  `).join('')}
+  `;
+}
