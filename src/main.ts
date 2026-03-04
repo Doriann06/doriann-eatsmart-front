03501b85a8ab094745233057d37dd2ed7ac3e27d
Doriann Bachelet <doriann.bak@gmail.com>
2026-03-04 22:53:08 +0100

    Besoin n°1 : Afficher les plats dans la console

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+interface Plat{
+  id:number;
+  nom:string;
+  prix:number;
+  description:string;
+}
+const carte:Plat[]=[
+  {id:1 ,nom:'Anchois 23cm',prix:7.9,description:"sauce tomate premium,origan,huile d'olive extra vierge,anchois,olive"},
+  {id:2 ,nom:'Emmental 23cm',prix:7.9,description:"sauce tomate premium,origan,huile d'olive extra vierge,emmental,basilic,olive"},
+  {id:3 ,nom:'Margherita 23cm',prix:7.9,description:"sauce tomate premium,origan,huile d'olive extra vierge,mozzarella"}
+]
+console.log(carte);
